fe16083359abe60b74e3bc1b02d0d33770d5379e
stalin <stalin.mora.burgos@gmail.com>
2024-10-15 17:20:45 -0500

    Add Rollo Ventana

--- Atlas/Telares/Pages/AddRollo.xaml.cs
+++ Atlas/Telares/Pages/AddRollo.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Telares
+namespace Telares.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddRollo.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddRollo : Window
     {
-        public MainWindow()
+        public AddRollo()
         {
             InitializeComponent();
         }
